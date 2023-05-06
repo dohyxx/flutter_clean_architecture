d722b083e397737d18ede7e67a84d4544fb08bc5
Dohyun <dohyun1225@gmail.com>
2023-05-06 11:25:47 +0900

    add image model & config

---
+++ lib/data/models/search_image_model.dart
@@ -0,0 +1,61 @@
+
+class SearchImageModel {
+  final int totalCount;
+  final int pageableCount;
+  final bool isEnd;
+
+  SearchImageModel({
+    required this.totalCount,
+    required this.pageableCount,
+    required this.isEnd,
+  });
+
+  factory SearchImageModel.fromJson(Map<String, dynamic> json) {
+    final jsonDocument = json['document'] as List;
+    final document = <ImageDocument>[];
+    for (var jsonDocument in jsonDocument) {
+      document.add(ImageDocument.fromJson(jsonDocument));
+    }
+
+    return SearchImageModel(
+      totalCount: json['meta']['total_count'],
+      pageableCount: json['meta']['pageable_count'],
+      isEnd: json['meta']['is_end'],
+    );
+  }
+}
+
+class ImageDocument {
+  final String collection;
+  final String thumbnailUrl;
+  final String imageUrl;
+  final int width;
+  final int height;
+  final String displaySitename;
+  final String docUrl;
+  final DateTime datetime;
+
+  ImageDocument({
+    required this.collection,
+    required this.thumbnailUrl,
+    required this.imageUrl,
+    required this.width,
+    required this.height,
+    required this.displaySitename,
+    required this.docUrl,
+    required this.datetime,
+  });
+
+  factory ImageDocument.fromJson(Map<String, dynamic> json) {
+    return ImageDocument(
+      collection: json['collection'],
+      thumbnailUrl: json['thumbnail_url'],
+      imageUrl: json['image_url'],
+      width: json['width'],
+      height: json['height'],
+      displaySitename: json['display_sitename'],
+      docUrl: json['doc_url'],
+      datetime: DateTime.parse(json['datetime']).toLocal(),
+    );
+  }
+}
---
+++ lib/data/sources/api_config.dart
@@ -0,0 +1,8 @@
+class ApiConfig {
+  ApiConfig._();
+
+  static const baseUrl = 'https://dapi.kakao.com';
+  static const headers = <String, String>{
+    'Authorization' : 'KakaoAK 875e9286d7a549b1968d08385a055756',
+  };
+}
