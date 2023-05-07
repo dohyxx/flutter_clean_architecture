17c76ef9245f0f9a822b03ade59bb10dea759b8b
Dohyun <dohyun1225@gmail.com>
2023-05-07 12:49:09 +0900

    add api client & request

---
+++ lib/data/sources/api_client.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_clean_architecture/data/models/search_image_model.dart';
+import 'package:flutter_clean_architecture/data/sources/api_config.dart';
+import 'package:flutter_clean_architecture/data/sources/api_request.dart';
+import 'package:http/http.dart';
+
+class ApiClient {
+  final Client client;
+  final ApiRequest _request;
+
+  ApiClient({
+    required this.client,
+    ApiRequest? request,
+  }) : _request = request ?? ApiRequest(client);
+
+  //이미지 검색 요청
+  Future<SearchImageModel> searchImage({
+    required String query,
+    String? sort,
+    int? page,
+    int? size,
+  }) async {
+    const url = '${ApiConfig.baseUrl}/v2/search/image';
+    final params = <String, dynamic>{};
+
+    params['query'] = query;
+
+    if (sort != null) {
+      params['sort'] = sort;
+    }
+    if (page != null) {
+      params['page'] = page;
+    }
+    if (size != null) {
+      params['size'] = size;
+    }
+
+    final result = await _request(
+      method: RequestMethod.get,
+      headers: ApiConfig.headers,
+      url: url,
+      params: params,
+    );
+
+    if (result.statusCode == 200) {
+      return SearchImageModel.fromJson(result.body);
+    } else {
+      throw PlatformException(
+        code: 'SYSYEM_ERROR',
+        details: {
+          'statusCode' : result.statusCode,
+          'boby' : result.body ?? '',
+        },
+      );
+    }
+  }
+}
---
+++ lib/data/sources/api_request.dart
@@ -0,0 +1,81 @@
+import 'dart:convert';
+
+import 'package:get/get.dart' as gets;
+import 'package:http/http.dart';
+
+
+enum RequestMethod { get }
+
+class ApiRequest {
+  final Client _client;
+
+  ApiRequest(Client client) : _client = client;
+
+  Future<ApiResponse> call({
+    required RequestMethod method,
+    required String url,
+    Map<String, String>? headers,
+    Map<String, dynamic>? params,
+  }) async {
+    gets.Get.log('[ApiRequest.call: $method, $url, ${params ?? '검색어 없음'}]');
+    final httpResponse = await _requestHttp(method, url, headers, params);
+    final response = ApiResponse.fromHttpResponse(httpResponse);
+
+    return response;
+  }
+
+
+  Future<Response> _requestHttp(
+      RequestMethod method,
+      String url,
+      Map<String, String>? headers,
+      Map<String, dynamic>? params,
+  ){
+    Future<Response> result;
+
+    switch(method){
+      case RequestMethod.get :
+        if(params?.isNotEmpty ?? false){
+          url += '?${_queryParameters(params!)}';
+        }
+        result = _client.get(
+          Uri.parse(url),
+          headers: headers,
+        );
+        break;
+    }
+
+    return result;
+  }
+
+  String _queryParameters(Map<String, dynamic> params){
+    return params.entries.map((e) => '${e.key}=${e.value}').join('&');
+  }
+
+}
+
+
+// 서버 응답 상태 체크
+class ApiResponse {
+  int statusCode;
+  dynamic body;
+
+  ApiResponse({
+    required this.statusCode,
+    this.body,
+  });
+
+  factory ApiResponse.fromHttpResponse(Response res) {
+    final response = ApiResponse(statusCode: res.statusCode);
+
+    if(res.body.isNotEmpty){
+      final decodeData = utf8.decode(res.bodyBytes);
+      response.body = jsonDecode(decodeData);
+    }
+
+    return response;
+  }
+}
+
+
+
